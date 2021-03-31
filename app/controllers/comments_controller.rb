73ae99f663222110376ce31f84c9fb2568e5dfa9
Vanessa Y <vyeung.wt@gmail.com>
2021-03-31 05:01:36 +0000

    add comment functionality to show view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def create   # Using the create method on @article.comments to create and save the comment. This will automatically link the comment so that it belongs to that particular article.
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article) # Back to original article which renders renders the show.html.erb
+  end
+ 
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
